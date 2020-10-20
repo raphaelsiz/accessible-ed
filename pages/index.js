23ad854e3eb5cf3468dd9d8030639884120e403b
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-20 15:35:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
+        <p color="black">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
