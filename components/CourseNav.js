9e50a5bc4c6b8ee76dbcd3beb64da7a0f2233dd8
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-20 22:04:13 -0700

    Update CourseNav.js

--- components/CourseNav.js
+++ components/CourseNav.js
@@ -9,7 +9,7 @@ export default function CourseNav(){
       <style jsx>{`
         div {
           width: 100%;
-          position: relative;
+          position: static;
           left: 0px;
           margin: 0px;
           padding: 0px;
