d6d09b3afc498340d0288c4c22e6e646180eee27
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-20 18:07:12 -0700

    Search CSS

--- components/Search.js
+++ components/Search.js
@@ -1,42 +1,19 @@
 import Link from 'next/link';
 export default function Searchbar({props}){
-/*  this.state = {
-    query: '',
-    results: {
-    },
-    loading: false,
-    message: ''
-  }*/
   return (
     <div className="container">
       <label className="search-label" htmlFor="search-input">
         <input type="text" value="" id="search-input" placeholder="search"></input>
       </label>
-      /*<style jsx>{`
-        div {
-          width: 100%;
-          position: absolute;
-          left: 0px;
+      <style jsx>{`
+        .container {
+          right: 10px;
           top: 0px;
           margin: 0px;
           padding: 0px;
           background: #1887b4;
         }
-        ul {
-          background: #1887b4;
-          list-style: none;
-          display: flex;
-        }
-        ul li {
-          color: white;
-          margin-right: 30px;
-        }
-        ul li a {
-          color: white;
-          text-decoration: none;
-        }
-      `} </style>*/
+      `} </style>
     </div>
-
   )
 }
