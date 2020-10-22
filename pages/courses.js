42e6040bb5f37eb136eab907f77db38ac91f0b53
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-21 17:01:27 -0700

    Update courses.js

--- pages/courses.js
+++ pages/courses.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '@components/Header'
+import CourseNav from '@components/CourseNav'
 import Footer from '@components/Footer'
 import Link from 'next/link'
 
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header title="Welcome to my app!" />
+        <CoursNav />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
