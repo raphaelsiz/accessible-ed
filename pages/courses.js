4298abdef86fe3c87beb4c15d5bb521fe2b10e2d
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-21 17:03:03 -0700

    typo whooops

--- pages/courses.js
+++ pages/courses.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <CoursNav />
+        <CourseNav />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
