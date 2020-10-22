ce51d5b90a00c5fcdac18f112fa191234573d1fe
raphaelsiz <64447714+raphaelsiz@users.noreply.github.com>
2020-10-21 17:06:40 -0700

    Update courses.js

--- pages/courses.js
+++ pages/courses.js
@@ -12,9 +12,6 @@ export default function Home() {
       </Head>
       <main>
         <CourseNav />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
